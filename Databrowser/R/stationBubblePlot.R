71bb8227518f9cccd7b7d1dbf9c28779c668774d
jonathan.s.callahan@gmail.com <jonathan.s.callahan@gmail.com>
2015-10-27 14:28:54 -0700

    Playing with bubble plot.

--- Databrowser/R/stationBubblePlot.R
+++ Databrowser/R/stationBubblePlot.R
@@ -20,48 +20,74 @@ if (FALSE) {
 
 stationBubblePlot <- function(dataList, infoList, textList) {
  
+  load('data_local/Mazama_station.RData')
+  library(png)
+  
+  #Replace the directory and file information with your info
+  ima <- readPNG("./data_local/RgoogleMaps_Seattle.png")
+  centerLat=47.620
+  centerLon=-122.320
+  halfHeight <- 0.068
+  halfWidth <- 0.100
+  xlo <- centerLon - halfWidth
+  xhi <- centerLon + halfWidth
+  ylo <- centerLat - halfHeight
+  yhi <- centerLat + halfHeight
+  
+  xvals <- seq(xlo,xhi,length.out=5)
+  yvals <- seq(ylo,yhi,length.out=5)
+  #Set up the plot area
+  plot(xvals,yvals, type='n', main="Plotting Over an Image", xlab="x", ylab="y")
+  
+  # Get the plot information so the image will fill the plot box, and draw it
+  usr <- par('usr')
+  rasterImage(ima, usr[1], usr[3], usr[2], usr[4])
+  
+  points(station$lon, station$lat, col='red', pch=15)
+
+
   # ----- Style ---------------------------------------------------------------
   
-  # NOTE:  RgoogleMaps max size is 640
-  size <- ifelse(infoList$plotWidth > 640, 640, infoList$plotWidth)
-  
-  # ----- Data Preparation ----------------------------------------------------
-    
-  # Get dataframe from the dataList
-  trip <- dataList$trip
-  station <- dataList$station
-  
-  fromStation <- table(trip$fromStationId)
-  station$fromCount <- as.numeric(fromStation[station$terminal])
-  toStation <- table(trip$toStation)
-  station$toCount <- as.numeric(toStation[station$terminal])
-  
-  # Remove 'Pronto Shop'
-  station <- subset(station,terminal != 'XXX-01')
-  oldPar <- par()
-  par(mar=c(5,20,4,2)+.1)
-  barplot(station$fromCount, horiz=TRUE, names.arg=station$name, las=2)
-  par(oldPar)
-  
-  rawUsage <- station$fromCount + station$toCount
-  usage <- rawUsage * station$onlineDays/max(station$onlineDays)
-  
-  breaks=quantile(usage,probs=seq(0,1,1/9))
-  # Give the first three breaks meaningful numbers
-  breaks[1:3] <- c(0,1,3)*365
-  usageIndex <- .bincode(usage,breaks=breaks,include.lowest=TRUE)
-  cols <- brewer.pal(max(usageIndex),'Spectral')[usageIndex]
-  cex <- 6 * station$usage/max(usage)
-  
-  
-  # ----- Plot ----------------------------------------------------------------
-
-  destfile <- paste0(infoList$outputDir,"/",infoList$outputFileBase,".png")
-  
-  map = GetMap(center = c(lat=47.6185, lon=-122.3282),
-               size=c(infoList$plotWidth,infoListp$plotWidth),
-               destfile=destfile,
-               zoom=8, format="png8")
+#   # NOTE:  RgoogleMaps max size is 640
+#   size <- ifelse(infoList$plotWidth > 640, 640, infoList$plotWidth)
+#   
+#   # ----- Data Preparation ----------------------------------------------------
+#     
+#   # Get dataframe from the dataList
+#   trip <- dataList$trip
+#   station <- dataList$station
+#   
+#   fromStation <- table(trip$fromStationId)
+#   station$fromCount <- as.numeric(fromStation[station$terminal])
+#   toStation <- table(trip$toStation)
+#   station$toCount <- as.numeric(toStation[station$terminal])
+#   
+#   # Remove 'Pronto Shop'
+#   station <- subset(station,terminal != 'XXX-01')
+#   oldPar <- par()
+#   par(mar=c(5,20,4,2)+.1)
+#   barplot(station$fromCount, horiz=TRUE, names.arg=station$name, las=2)
+#   par(oldPar)
+#   
+#   rawUsage <- station$fromCount + station$toCount
+#   usage <- rawUsage * station$onlineDays/max(station$onlineDays)
+#   
+#   breaks=quantile(usage,probs=seq(0,1,1/9))
+#   # Give the first three breaks meaningful numbers
+#   breaks[1:3] <- c(0,1,3)*365
+#   usageIndex <- .bincode(usage,breaks=breaks,include.lowest=TRUE)
+#   cols <- brewer.pal(max(usageIndex),'Spectral')[usageIndex]
+#   cex <- 6 * station$usage/max(usage)
+#   
+#   
+#   # ----- Plot ----------------------------------------------------------------
+# 
+#   destfile <- paste0(infoList$outputDir,"/",infoList$outputFileBase,".png")
+#   
+#   map = GetMap(center = c(lat=47.6185, lon=-122.3282),
+#                size=c(infoList$plotWidth,infoListp$plotWidth),
+#                destfile=destfile,
+#                zoom=8, format="png8")
   
 #  PlotOnStaticMap(map, station$lat, station$lon, destfile=destfile,
 #                  NEWMAP=FALSE, TrueProj=FALSE,
